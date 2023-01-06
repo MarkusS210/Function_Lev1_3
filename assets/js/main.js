61bb30e96d9c96280171545f1ed99dc85cd8d5e8
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-06 13:12:54 +0100

    Function_Lev1_3

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+function intro3(name, stadt, alter){
+    name = "Markus Schneider."
+    alter = 33
+    stadt = "Moers."
+console.log("Hallo, mein Name ist " + name + "Ich bin " + alter + " Jahre alt." + " Ich komme aus " + stadt);
+}
+
+intro3()
